554a4c1e823830bddf5988af9528bcffd0b60eba
grumpyDv <stevenreaper@gmail.com>
2025-10-19 15:15:20 +0200

    CH2 L3 FPS finished

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.time import Clock
 from constants import *
 
 
@@ -6,6 +7,8 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -13,6 +16,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
